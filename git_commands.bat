c63c0ce889dc8287dc39fbb8196053189a54686d
Pandey <tarun.pandey@accenture.com>
2016-08-11 16:14:15 +0530

    adding src folder from batch file

--- git_commands.bat
+++ git_commands.bat
@@ -4,7 +4,7 @@ git checkout master
 git status
 git add .
 git commit -m "adding src folder from batch file"
-git remote add cloudOpsNew https://tarunpandey:Aug2016@github.com/tarunpandey/CodeRepo.git
+git remote add cloudOpsNew https://tarunpandey:Aug2016@github.com/tarunpandey/MainRepo.git
 git push -u cloudOpsNew master
 
 pause
